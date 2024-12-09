681949331bb46f2c6832370d53bb8a1fd7e12abf
sekihin <sekihin.net@gmail.com>
2024-12-09 19:54:19 +0800

    Prisma

---
+++ config/config.ts
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+config()
+export const SERVER_PORT = process.env.SERVER_PORT
+export const DATABASE_URL = process.env.DATABASE_URL
+export const AUTH_SECRET = process.env.AUTH_SECRET
